b287b3888a3d04a89427526d46f3b27c59aeec75
Stratis Dermanoutsos <stratis450@gmail.com>
2025-07-12 21:31:36 +0300

    Live example no.1

--- examples/MapperLite.Demo.Console/Program.cs
+++ examples/MapperLite.Demo.Console/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MapperLite.Configuration;
+using MapperLite.Demo.Models.Dto;
+using MapperLite.Demo.Models.Persistence;
+using MapperLite.Demo.Profiles;
 
-Console.WriteLine("Hello, World!");
+// Create the MapperConfiguration
+var config = new MapperConfiguration();
+
+// Create the mapping profiles
+var userProfile = new UserProfile();
+var userAddressProfile = new UserAddressProfile();
+
+// Configure the profiles with the MapperConfiguration
+userProfile.Configure(config);
+userAddressProfile.Configure(config);
+
+// Create the Mapper instance with the configuration
+var mapper = new MapperLite.Mapper(config);
+
+var user = new User
+{
+    Id = 1,
+    FirstName = "John",
+    LastName = "Doe",
+    Addresses = [
+        new UserAddress { Id = 1, Street = "Main St", Number = "123", City = "Anytown", ZipCode = "12345" },
+        new UserAddress { Id = 2, Street = "Elm St", City = "Othertown", ZipCode = "67890" }
+    ]
+};
+
+var userDto = mapper.Map<UserReadDto>(user);
+// var userDto = mapper.Map<User, UserReadDto>(user); // Equal to the above line
+
+// Output the results
+Console.WriteLine(user);
+Console.WriteLine(userDto);
