222a864a7bf0dcf3838ada06fa0646d2d13ae2b4
Stratis Dermanoutsos <stratis450@gmail.com>
2025-07-12 21:13:31 +0300

    Fixed wrong call

--- src/MapperLite/IMapper.cs
+++ src/MapperLite/IMapper.cs
@@ -2,14 +2,14 @@ namespace MapperLite;
 
 public interface IMapper
 {
+    TDestination Map<TDestination>(object source)
+        where TDestination : class;
+
     TDestination Map<TSource, TDestination>(TSource source)
         where TSource : class
         where TDestination : class;
 
     TDestination MapPartial<TSource, TDestination>(TSource source, TDestination destination)
         where TSource : class
-        where TDestination : class;
-
-    TDestination Map<TDestination>(object source)
         where TDestination : class;
 }
--- src/MapperLite/Mapper.cs
+++ src/MapperLite/Mapper.cs
@@ -10,16 +10,18 @@ public class Mapper(MapperConfiguration config) : IMapper
         ArgumentNullException.ThrowIfNull(source);
 
         var sourceType = source.GetType();
-        var mapMethod = (typeof(Mapper)
-                            .GetMethod(nameof(Map), [ sourceType ]) // look for Map<TSource, TDestination>(TSource source)
-                        ?? typeof(Mapper).GetMethods()
-                            .FirstOrDefault(m =>
-                                m is { Name: nameof(Map), IsGenericMethodDefinition: true }
-                                && m.GetParameters().Length == 1
-                                && m.GetGenericArguments().Length == 2))
-                        ?? throw new InvalidOperationException("Could not find generic Map method.");
 
-        var genericMethod = mapMethod.MakeGenericMethod(sourceType, typeof(TDestination)) ?? throw new InvalidOperationException();
+        // Find the Map<TSource, TDestination>(TSource source) method
+        var mapMethod = typeof(Mapper)
+            .GetMethods()
+            .FirstOrDefault(m =>
+                m is { Name: nameof(Map), IsGenericMethodDefinition: true }
+                && m.GetGenericArguments().Length == 2
+                && m.GetParameters().Length == 1)
+                        ?? throw new InvalidOperationException("Could not find generic Map<TSource, TDestination> method.");
+
+        var genericMethod = mapMethod.MakeGenericMethod(sourceType, typeof(TDestination));
+
         return (TDestination?)genericMethod.Invoke(this, [source])
                ?? throw new InvalidOperationException($"Mapping from {sourceType} to {typeof(TDestination)} returned null.");
     }
